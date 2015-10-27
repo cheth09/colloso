30be0049b85818a8354b09396eb33668e25b8992
cheth09 <chethan@codecraft.co.in>
2015-10-27 16:52:54 +0530

    Update startappium.bat
    server port changes

--- startappium.bat
+++ startappium.bat
@@ -1,2 +1,2 @@
-"C:/Program Files (x86)/Appium/node.exe" "C:/Program Files (x86)/Appium/node_modules/appium/bin/Appium.js" --address 127.0.0.1 --bootstrap-port 4723 --no-reset --local-timezone
+"C:/Program Files (x86)/Appium/node.exe" "C:/Program Files (x86)/Appium/node_modules/appium/bin/Appium.js" --address 127.0.0.1 --bootstrap-port 4725 --no-reset --local-timezone
 java -cp E:\newspace\Appium_Demo\lib\*;E:\newspace\Appium_Demo\bin org.testng.TestNG testng.xml
