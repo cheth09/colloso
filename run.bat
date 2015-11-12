d88bffbc446b15a108f2fea89b766623299975fd
cheth09 <chethan@codecraft.co.in>
2015-11-12 12:46:49 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 timeout /t 20 /nobreak
 java -cp "E:\newspace\Appium_Demo\lib\*;E:\newspace\Appium_Demo\bin" org.testng.TestNG testng.xml
-=======
-timeout /t 10 /nobreak
-java -cp "E:\newspace\Appium_Demo\lib\*;E:\newspace\Appium_Demo\bin" org.testng.TestNG testng.xml
->>>>>>> origin/master
